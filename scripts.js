ba686bce6be503c420c2d2828987a75a7ae33483
Carlos Lukeni <carloslukeni110@gmail.com>
2024-10-24 21:51:26 +0100

    Removi o arquivo calcular.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,6 @@
-const nome = document.getElementById("nome");
-const data = document.getElementById("data");
-const imagem = document.getElementById("Imagem");
-const h2 = document.querySelector("h2");
-const h3 = document.querySelector("h3");
-const img = document.getElementById("imgusr");
+const input1 = document.getElementById("nome");
+const nome = document.getElementById("nome-usr");
 
 
-nome.addEventListener("keyup", display);
-data.addEventListener("keyup", display2);
-
-
-function display(){
-      h2.innerHTML = nome.value;
-}
-
-function display2(){
-      h3.innerHTML = data.value;
-}
-
-imagem.addEventListener('change', function(event) {
-      const arquivo = event.target.files[0];
-      if (arquivo) {
-        const leitor = new FileReader();
-        
-        leitor.onload = function(e) {
-          imgusr.src = e.target.result;
-          imgusr.style.display = 'block';
-        }
-
-        leitor.readAsDataURL(arquivo);
-      }
-    });
           
 
