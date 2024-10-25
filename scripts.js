0d916a0ff653527d94ac45befb30f1cab0c9e4ea
Carlos Lukeni <carloslukeni110@gmail.com>
2024-10-25 09:53:15 +0100

    done

--- scripts.js
+++ scripts.js
@@ -1,5 +1,75 @@
-const input1 = document.getElementById("nome");
-const nome = document.getElementById("nome-usr");
+
+const input = document.getElementById("nome");
+const input2 = document.getElementById("data");
+
+
+
+
+
+
+function display(){
+      localStorage.setItem( 'nome' , input.value);
+      localStorage.setItem('data', input2.value);
+
+      alert('Dados enviados com sucesso!');
+
+}
+
+    // Função para recuperar os dados do localStorage ao carregar a página
+
+      window.onload = function() {
+      const nomeSalvo = localStorage.getItem('nome');
+      const dataSalva = localStorage.getItem('data');
+
+      
+
+      if (nomeSalvo) {
+        document.getElementById('nome') == nomeSalvo;
+      }
+      if (dataSalva) {
+        document.getElementById('data') == dataSalva;
+      }
+    };
+
+// Função para salvar a imagem no localStorage
+/*function salvarImagem() {
+      const input = document.getElementById('inputImagem');
+      const file = input.files[0];
+
+      if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+          const base64String = event.target.result;
+          localStorage.setItem('imagemSalva', base64String);
+          consolo.log(localStorage.getItem("imagemSalva"));
+          exibirImagem(base64String);  // Exibir imagem imediatamente após salvar
+        };
+
+        reader.readAsDataURL(file); // Converte o arquivo em base64
+      } else {
+        alert('Nenhuma imagem selecionada.');
+      }
+    }
+
+    // Função para exibir a imagem
+    function exibirImagem(base64String) {
+      const imagemExibida = document.getElementById('imagemExibida');
+      imagemExibida.src = base64String;
+    }
+      */
+
+    // Recupera a imagem do localStorage ao carregar a página
+   //  window.onload = function() {
+    //   const imagemSalva = localStorage.getItem('imagemSalva');
+   //    if (imagemSalva) {
+   //      exibirImagem(imagemSalva);
+   //    }
+  //   };
+
+
+
+
 
 
           
