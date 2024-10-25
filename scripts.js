6968151e6452573c4621802346926c1700314716
Carlos Lukeni <carloslukeni110@gmail.com>
2024-10-25 15:12:34 +0100

    Tudo finalizado

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
+<<<<<<< HEAD
 const input1 = document.getElementById("nome");
 const nome = document.getElementById("nome-usr");
 
 
           
 
+=======
+>>>>>>> parent of 122be3e (criei o calcular.js)
