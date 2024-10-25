8d9d494ca1051e7b78a9c3c3a34102cfa639ee97
Carlos Lukeni <carloslukeni110@gmail.com>
2024-10-25 15:28:05 +0100

    ggv

--- scripts.js
+++ scripts.js
@@ -11,11 +11,10 @@ function display(){
       localStorage.setItem( 'nome' , input.value);
       localStorage.setItem('data', input2.value);
 
-      alert('Dados enviados com sucesso!');
+
 
 }
 
-    // Função para recuperar os dados do localStorage ao carregar a página
 
       window.onload = function() {
       const nomeSalvo = localStorage.getItem('nome');
@@ -31,43 +30,44 @@ function display(){
       }
     };
 
-// Função para salvar a imagem no localStorage
-/*function salvarImagem() {
+    function apagar(){
+      localStorage.clear();
+    }
+    
+    function salvarImagem() {
       const input = document.getElementById('inputImagem');
-      const file = input.files[0];
+      const ficheiros = input.ficheiros[0];
 
-      if (file) {
+      if (ficheiros) {
         const reader = new FileReader();
 
         reader.onload = function(event) {
           const base64String = event.target.result;
           localStorage.setItem('imagemSalva', base64String);
-          consolo.log(localStorage.getItem("imagemSalva"));
+          alert('Imagem salva com sucesso!');
           exibirImagem(base64String);  // Exibir imagem imediatamente após salvar
         };
 
-        reader.readAsDataURL(file); // Converte o arquivo em base64
+        reader.readAsDataURL(ficheiros); // Converte o arquivo em base64
       } else {
         alert('Nenhuma imagem selecionada.');
       }
     }
 
-    // Função para exibir a imagem
+    /* Função para exibir a imagem
     function exibirImagem(base64String) {
       const imagemExibida = document.getElementById('imagemExibida');
       imagemExibida.src = base64String;
     }
-      */
 
-    // Recupera a imagem do localStorage ao carregar a página
-   //  window.onload = function() {
-    //   const imagemSalva = localStorage.getItem('imagemSalva');
-   //    if (imagemSalva) {
-   //      exibirImagem(imagemSalva);
-   //    }
-  //   };
-
-
+*/
+   
+    window.onload = function() {
+      const imagemSalva = localStorage.getItem('imagemSalva');
+      if (imagemSalva) {
+        exibirImagem(imagemSalva);
+      }
+    };
 
 
 
